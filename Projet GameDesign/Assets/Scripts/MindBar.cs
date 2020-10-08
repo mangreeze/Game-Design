206f905242e60b606940592b18f237e9af88cbad
clément <clement.becamel@epitech.eu>
2020-10-09 00:35:11 +0200

    Add MindBar

---
+++ Projet GameDesign/Assets/Scripts/MindBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MindBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxMind(int mind)
+    {
+        slider.maxValue = mind;
+        slider.value = mind;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetMind(int mind)
+    {
+        slider.value = mind;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void LoseOneMind()
+    {
+        if (slider.value > 0) {
+            slider.value = slider.value - 1;
+            fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+    }
+}
---
+++ Projet GameDesign/Assets/Scripts/PlayerMind.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMind : MonoBehaviour
+{
+    public bool dark = true;
+    public float waitTime = 1.0f;
+    private float timer = 0.0f;
+    public int maxMind = 100;
+    public int currentMind;
+    public int addMind = 5;
+
+    public MindBar mindBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMind = maxMind;
+        mindBar.SetMaxMind(maxMind);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            if (dark && currentMind > 0)
+            {
+                currentMind = currentMind - 1;
+                mindBar.LoseOneMind();
+            } else
+            {
+                currentMind += 5;
+                if (currentMind > 100)
+                {
+                    currentMind = 100;
+                }
+            }
+            timer -= waitTime;
+        }
+        mindBar.SetMind(currentMind);
+    }
+
+    void RecoverMind(int mindRecover)
+    {
+        if (currentMind + mindRecover > 100)
+        {
+            currentMind = 100;
+            mindBar.SetMind(currentMind);
+        } else {
+            currentMind += mindRecover;
+            mindBar.SetMind(currentMind);
+        }
+    }
+
+    void SetDarkVariable(bool newDark)
+    {
+        dark = newDark;
+    }
+}
